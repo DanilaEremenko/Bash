a34cd602ab0408026613538fb6a66e1116c8c17d
danila <danilaeremenko@mail.ru>
2018-10-22 19:34:25 +0300

    1.sh done

--- OS/t.sh
+++ OS/t.sh
@@ -1,20 +1,10 @@
 #! /bin/bash
 
+log=false;
 
-#i=3;
-
-#if [ $(($i % 3)) == 0 ];
-#then 
-#echo "true";
-#else 
-#echo "false";
-#fi
-
-i=1;
-
-if [[ $(echo abc | cut -c$i) == a ]]
-then 
-echo true;
-else 
-echo false;
+if $log 
+then
+	echo TRUE;
+else
+	echo FALSE;
 fi
