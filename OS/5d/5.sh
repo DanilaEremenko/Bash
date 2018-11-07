03afcab080ca7c766ff1961410642bba98b27433
DanilaEremenko <danilaeremenko@mail.ru>
2018-11-07 15:50:53 +0300

    kruto

--- OS/5d/5.sh
+++ OS/5d/5.sh
@@ -3,6 +3,6 @@
 
 for type in b c d p f l s d
 do
-find $1 -type $type -ls > "type_$type";
+	find $1 -type $type -ls > "type_$type";
 
 done 
--- OS/6d/6.sh
+++ OS/6d/6.sh
@@ -1,1 +1,9 @@
-#! /bin/bash
+#!/bin/bash
+echo "without flags---------------------------" > $2;
+od $1 >> $2;
+for flag in a b c d f
+do
+    echo "flag = -$flag---------------------------" >> $2;
+    od -$flag $1 >> $2;
+
+done
